0e8b6e9b0ffefec13b3e0bdfd0d4723f9a5dc2e9
Suman Moond <62123995+suman0771@users.noreply.github.com>
2022-01-16 13:05:09 +0530

    Add files via upload

---
+++ basic/arithmeticOperator.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+	int x=8;
+	int y=9;
+	float z=9
+	int sum=x+y;
+	cout<<sum;
+
+	int sub = x-y;
+	cout<<sub;
+
+	int mul=x*y;
+	cout<<mul;
+
+	int div= x/y; //0 as ans will be 0.... so prints int 
+	cout<<div;
+
+	int div1=x/z;  //float result
+	cout<<div1;
+
+	int mod =x%y; //remainder of a division x/y
+	cout<<mod;
+	//cannot use float for module
+
+	x +=2;
+	cout<<x;
+
+	x++;    //here x= 10 and value of x get updated from next line bcz of post increament
+	cout<<x; ///add 1 to x
+
+	x--;
+	cout<<x;   //remove 1 from x
+
+}
+
+/*
+Order of operations:
+
+B
+E
+D
+M
+A
+S
+
+*/
+
+/*
+Arthmetic Operators
+
++
+-
+*
+/
+%
+++
+--
+
+*/
